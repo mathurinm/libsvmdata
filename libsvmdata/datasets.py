882430e112242ac8234f61de3696d9f3551a9e7c
mathurinm <mathurin.massias@gmail.com>
2020-10-27 11:43:02 +0100

    add small regression dataset, test 4 types of datasets

--- libsvmdata/datasets.py
+++ libsvmdata/datasets.py
@@ -16,55 +16,69 @@ from sklearn.datasets import load_svmlight_file
 LIBSVMDATA_PATH = pjoin(str(Path.home()), 'libsvm_data/')
 
 
-NAMES = {'finance': 'regression/log1p.E2006.train',
-         'kdda_train': 'binary/kdda',
-         'news20': 'binary/news20.binary',
-         'rcv1_topics_test': 'multilabel/rcv1_topics_test_2.svm',
-         'rcv1_train': 'binary/rcv1_train.binary',
-         'real-sim': 'binary/real-sim',
-         'sector_train': 'multiclass/sector/sector',
-         'sector_test': 'multiclass/sector/sector.t',
-         'url': 'binary/url_combined',
-         'webspam': 'binary/webspam_wc_normalized_trigram.svm',
-         }
+NAMES = {
+    'bodyfat': 'regression/bodyfat',
+    'finance': 'regression/log1p.E2006.train.bz2',
+    'iris': 'multiclass/iris.scale',
+    'kdda_train': 'binary/kdda.bz2',
+    'news20': 'binary/news20.binary.bz2',
+    'rcv1_topics_test': 'multilabel/rcv1_topics_test_2.svm.bz2',
+    'rcv1_train': 'binary/rcv1_train.binary.bz2',
+    'real-sim': 'binary/real-sim.bz2',
+    'sector_train': 'multiclass/sector/sector.bz2',
+    'sector_test': 'multiclass/sector/sector.t.bz2',
+    'url': 'binary/url_combined.bz2',
+    'webspam': 'binary/webspam_wc_normalized_trigram.svm.bz2',
+}
 
-N_FEATURES = {'finance': 4272227,
-              'kdda_train': 20216830,
-              'news20': 1355191,
-              'rcv1_topics_test': 47236,
-              'rcv1_train': 47236,
-              'real-sim': 20958,
-              'sector_train': 55197,
-              'sector_test': 55197,
-              'url': 3231961,
-              'webspam': 16609143,
-              }
+N_FEATURES = {
+    'bodyfat': 14,
+    'finance': 4272227,
+    'iris': 4,
+    'kdda_train': 20216830,
+    'news20': 1355191,
+    'rcv1_topics_test': 47236,
+    'rcv1_train': 47236,
+    'real-sim': 20958,
+    'sector_train': 55197,
+    'sector_test': 55197,
+    'url': 3231961,
+    'webspam': 16609143,
+}
 
 
 def download_libsvm(dataset, destination, replace=False):
     """Download a dataset from LIBSVM website."""
     url = ("https://www.csie.ntu.edu.tw/~cjlin/libsvmtools/datasets/" +
            NAMES[dataset])
-    path = download(url + '.bz2', destination, replace=replace)
+    path = download(url, destination, replace=replace)
     return path
 
 
-def _get_X_y(dataset, compressed_path, multilabel, replace=False):
+def _get_X_y(dataset, source_path, multilabel, replace=False):
     """Load a LIBSVM dataset as sparse X and observation y/Y.
     If X and y already exists as npz and npy, they are not redownloaded unless
     replace=True."""
 
-    ext = '.npz' if multilabel else '.npy'
-    y_path = pjoin(LIBSVMDATA_PATH, "%s_target%s" % (NAMES[dataset], ext))
-    X_path = pjoin(LIBSVMDATA_PATH, "%s_data.npz" % NAMES[dataset])
-    if replace or not os.path.isfile(y_path) or not os.path.isfile(X_path):
-        tmp_path = pjoin(LIBSVMDATA_PATH, "%s" % NAMES[dataset])
+    # some files are compressed, some are not:
+    if NAMES[dataset].endswith('.bz2'):
+        stripped_name = NAMES[dataset][:-4]
+    else:
+        stripped_name = NAMES[dataset]
 
-        decompressor = BZ2Decompressor()
-        print("Decompressing...")
-        with open(tmp_path, "wb") as f, open(compressed_path, "rb") as g:
-            for data in iter(lambda: g.read(100 * 1024), b''):
-                f.write(decompressor.decompress(data))
+    ext = '.npz' if multilabel else '.npy'
+    y_path = pjoin(LIBSVMDATA_PATH, "%s_target%s" % (stripped_name, ext))
+    X_path = pjoin(LIBSVMDATA_PATH, "%s_data.npz" % stripped_name)
+    if replace or not os.path.isfile(y_path) or not os.path.isfile(X_path):
+        tmp_path = pjoin(LIBSVMDATA_PATH, "%s" % stripped_name)
+
+        # decompress file only if it is compressed
+        if NAMES[dataset].endswith('.bz2'):
+            decompressor = BZ2Decompressor()
+            print("Decompressing...")
+            with open(tmp_path, "wb") as f, open(source_path, "rb") as g:
+                for data in iter(lambda: g.read(100 * 1024), b''):
+                    f.write(decompressor.decompress(data))
 
         n_features_total = N_FEATURES[dataset]
         print("Loading svmlight file...")
@@ -122,7 +136,7 @@ def fetch_libsvm(dataset, replace=False, normalize=False, min_nnz=3):
     Returns
     -------
     X : scipy.sparse.csc_matrix
-        Design matrix.
+        Design matrix, in column sparse format.
 
     y : 1D or 2D np.array
         Design vector or matrix (in multiclass setting)
@@ -147,10 +161,10 @@ def fetch_libsvm(dataset, replace=False, normalize=False, min_nnz=3):
     is_regression = NAMES[dataset].split('/')[0] == 'regression'
 
     print("Dataset: %s" % dataset)
-    compressed_path = pjoin(LIBSVMDATA_PATH, "%s.bz2" % NAMES[dataset])
-    download_libsvm(dataset, compressed_path, replace=replace)
+    destination_path = pjoin(LIBSVMDATA_PATH, NAMES[dataset])
+    download_libsvm(dataset, destination_path, replace=replace)
 
-    X, y = _get_X_y(dataset, compressed_path, multilabel, replace=replace)
+    X, y = _get_X_y(dataset, destination_path, multilabel, replace=replace)
 
     # preprocessing
     if min_nnz != 0:
@@ -167,4 +181,5 @@ def fetch_libsvm(dataset, replace=False, normalize=False, min_nnz=3):
 
 if __name__ == "__main__":
     for dataset in NAMES:
-        fetch_libsvm(dataset, replace=False)
+        if not dataset.startswith("sector"):
+            fetch_libsvm(dataset, replace=False)
--- libsvmdata/tests/test_datasets.py
+++ libsvmdata/tests/test_datasets.py
@@ -1,14 +1,42 @@
-import numpy as np
 import pytest
+import numpy as np
 
 from libsvmdata import fetch_libsvm
-from libsvmdata.datasets import NAMES
 
 
-@pytest.mark.parametrize("name", NAMES.keys())
-def test_datasets(name):
-    if "sector" in name:
-        pytest.xfail(name)
+# TODO: test all datasets without timeout
+# @pytest.mark.parametrize("name", dataset.NAMES.keys())
+# def test_datasets(name):
+#     if "sector" in name:
+#         pytest.xfail(name)
 
-    X, y = fetch_libsvm(name)
+#     X, y = fetch_libsvm(name)
+#     np.testing.assert_equal(X.shape[0], y.shape[0])
+
+
+def test_binary():
+    # download if not present:
+    X, y = fetch_libsvm("news20")
     np.testing.assert_equal(X.shape[0], y.shape[0])
+    # also checks that loading saved files works:
+    X, y = fetch_libsvm("news20")
+
+
+def test_multilabel():
+    # test download
+    X, Y = fetch_libsvm("rcv1_topics_test")
+    np.testing.assert_equal(X.shape[0], Y.shape[0])
+    # test saved npz loading
+    X, Y = fetch_libsvm("rcv1_topics_test")
+
+
+def test_regression():
+    X, y = fetch_libsvm("bodyfat")
+    np.testing.assert_equal(X.shape[0], y.shape[0])
+    X, y = fetch_libsvm("bodyfat")
+
+
+def test_multiclass():
+    X, y = fetch_libsvm("iris")
+    np.testing.assert_equal(X.shape[0], y.shape[0])
+    X, y = fetch_libsvm("iris")
