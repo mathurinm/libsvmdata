0950ed75a5c4bffd0fc860b96e232628b551c4d8
Theo Guyard <theo.guyard@insa-rennes.fr>
2023-07-10 17:31:47 +0200

    Fix tests with allclose comparison to all-zero array

--- libsvmdata/tests/test_datasets.py
+++ libsvmdata/tests/test_datasets.py
@@ -66,5 +66,5 @@ def test_multilabel(dataset_name, n, p):
 def test_normalization():
     X, y = fetch_libsvm("abalone", normalize=True)
     assert_allclose(np.linalg.norm(X, axis=0), 1.)
-    assert_allclose(np.mean(y), 0.)
+    assert_allclose(np.mean(y), 0., atol=1e-07)
     assert_allclose(np.std(y), 1.)
