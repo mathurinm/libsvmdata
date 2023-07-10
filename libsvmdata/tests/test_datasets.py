a9233755334da97c2f50b101b469499526cebf9b
Theo Guyard <theo.guyard@insa-rennes.fr>
2023-07-10 15:13:17 +0200

    Change to assert_* function

--- libsvmdata/tests/test_datasets.py
+++ libsvmdata/tests/test_datasets.py
@@ -1,6 +1,6 @@
 import pytest
 import numpy as np
-from numpy.testing import assert_
+from numpy.testing import assert_, assert_allclose, assert_equal
 from libsvmdata import fetch_libsvm
 
 TEST_DATASETS = {
@@ -27,28 +27,28 @@ TEST_DATASETS = {
 @pytest.mark.parametrize("dataset_name,n,p", TEST_DATASETS["regression"])
 def test_regression(dataset_name, n, p):
     X, y = fetch_libsvm(dataset_name)
-    assert_(X.shape[0] == n)
-    assert_(X.shape[1] == p)
-    assert_(y.shape[0] == n)
+    assert_equal(X.shape[0], n)
+    assert_equal(X.shape[1], p)
+    assert_equal(y.shape[0], n)
 
 
 @pytest.mark.filterwarnings("ignore:FutureWarning")
 @pytest.mark.parametrize("dataset_name,n,p", TEST_DATASETS["binary"])
 def test_binary(dataset_name, n, p):
     X, y = fetch_libsvm(dataset_name, n, p)
-    assert_(X.shape[0] == n)
-    assert_(X.shape[1] == p)
-    assert_(y.shape[0] == n)
-    assert_(len(np.unique(y)) == 2)
+    assert_equal(X.shape[0], n)
+    assert_equal(X.shape[1], p)
+    assert_equal(y.shape[0], n)
+    assert_equal(len(np.unique(y)), 2)
 
 
 @pytest.mark.filterwarnings("ignore:FutureWarning")
 @pytest.mark.parametrize("dataset_name,n,p", TEST_DATASETS["multiclass"])
 def test_multiclass(dataset_name, n, p):
     X, y = fetch_libsvm(dataset_name)
-    assert_(X.shape[0] == n)
-    assert_(X.shape[1] == p)
-    assert_(y.shape[0] == n)
+    assert_equal(X.shape[0], n)
+    assert_equal(X.shape[1], p)
+    assert_equal(y.shape[0], n)
     assert_(len(np.unique(y)) > 2)
 
 
@@ -56,15 +56,15 @@ def test_multiclass(dataset_name, n, p):
 @pytest.mark.parametrize("dataset_name,n,p", TEST_DATASETS["multilabel"])
 def test_multilabel(dataset_name, n, p):
     X, y = fetch_libsvm(dataset_name)
-    assert_(X.shape[0] == n)
-    assert_(X.shape[1] == p)
-    assert_(y.shape[0] == n)
+    assert_equal(X.shape[0], n)
+    assert_equal(X.shape[1], p)
+    assert_equal(y.shape[0], n)
     assert_(y.shape[1] > 2)
 
 
 @pytest.mark.filterwarnings("ignore:FutureWarning")
 def test_normalization():
     X, y = fetch_libsvm("abalone", normalize=True)
-    assert_(np.allclose(np.linalg.norm(X, axis=0), 1.))
-    assert_(np.allclose(np.mean(y), 0.))
-    assert_(np.allclose(np.std(y), 1.))
+    assert_allclose(np.linalg.norm(X, axis=0), 1.)
+    assert_allclose(np.mean(y), 0.)
+    assert_allclose(np.std(y), 1.)
